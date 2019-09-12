5452c01f607945f88440f608bdda6136557db638
Shael Minuk <shael.minuk@gmail.com>
2019-09-12 19:06:40 -0400

    creating HFSS

---
+++ createHFSSProj.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 13:07:30 2019
+
+@author: shael
+"""
+
+import ScriptEnv
+
+ScriptEnv.Initialize("Ansoft.ElectronicsDesktop")
+oDesktop.RestoreWindow()
+oProject = oDesktop.NewProject()
